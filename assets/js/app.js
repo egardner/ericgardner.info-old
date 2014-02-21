2b7b4ae8461a3f3559225629a3143627a54d6dbb
egardner <egardner@Erics-MacBook-Pro.local>
2014-02-21 09:18:40 -0800

    Misc updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,14 @@ $(window).load(function() {
 	touch: true
 	});
 });
+
+// Initialize Skrollr for parallax
+( function( $ ) {
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+            Debugging - Log the current scroll position.
+            console.log(data.curTop);
+        }
+    });
+} )( jQuery );
