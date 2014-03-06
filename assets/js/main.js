10470fbc561a78b43fb09e4c4ac82f55170b1ec0
egardner <egardner@Erics-MacBook-Pro.local>
2014-03-05 22:57:00 -0800

    Skrollr working on desktop but not mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,52 @@
 // from: http://codepen.io/bradfrost/pen/sHvaz
 
 $(document).ready(function() {
-  $('body').addClass('js');
-  var $menu = $('#menu'),
+
+
+	$window = $(window);
+	$slide = $('.homeSlide');
+	$body = $('body');
+	
+    //FadeIn all sections   
+	$body.imagesLoaded( function() {
+		setTimeout(function() {
+		      
+		      // Resize sections
+		      adjustWindow();
+		      
+		      // Fade in sections
+			  $body.removeClass('loading').addClass('loaded');
+			  
+		}, 800);
+	});
+	
+	function adjustWindow(){
+		
+		// Init Skrollr
+		
+		
+		// Get window size
+	    winH = $window.height();
+	    
+	    // Keep minimum height 550
+	    if(winH <= 550) {
+			winH = 550;
+		} 
+	    
+	    // Resize our slides
+	    $slide.height(winH);
+	    
+	    // Refresh Skrollr after resizing our sections
+	    
+	    
+	}
+
+
+
+
+	// responsive menu scripts
+	$('body').addClass('js');
+  	var $menu = $('#menu'),
     $menulink = $('.menu-link');
   
 	$menulink.click(function() {
@@ -11,6 +55,14 @@ $(document).ready(function() {
 	  $menu.toggleClass('active');
 	  return false;
 	});
+
+	// initialize skrollr
+	var s = skrollr.init({          
+            mobileCheck: function() {
+                //hack - forces mobile version to be off
+                return false;
+            }
+        });
 
 });
 
