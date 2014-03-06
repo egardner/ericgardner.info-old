c870072aa885ef3a6806152db9dbed9e95dacd9a
egardner <egardner@ITTEMPs-Mac-Pro.local>
2014-03-06 14:31:13 -0800

    Photoessay updates
    Skrollr now working on photoessay section.
    Main.js now contains helper jQuery for image resizing.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,13 +3,22 @@
 
 $(document).ready(function() {
 
-
-
-
 	// responsive menu scripts
 	$('body').addClass('js');
   	var $menu = $('#menu'),
-    $menulink = $('.menu-link');
+    	$menulink = $('.menu-link');
+
+	// full width images
+	// css 100vh 100vw specifies starting image size
+	// jquery helps when mobile viewport rotates, etc.
+	$(window).resize(function() {
+		var viewportWidth = $(window).width();
+		var viewportHeight = $(window).height();
+		$('.slide-background').css("width", viewportWidth);
+		$('.slide-background').css("height", viewportHeight);
+
+	});
+
   
 	$menulink.click(function() {
 	  $menulink.toggleClass('active');
